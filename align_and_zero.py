ba6c72967556c0f7d2fbb7800117c0e290d6a6f1
jminardi <jack@minardi.org>
2014-05-14 17:40:22 -0400

    Added in Profile array

--- align_and_zero.py
+++ align_and_zero.py
@@ -2,6 +2,7 @@ from mecode.devices.keyence_profilometer import KeyenceProfilometer
 from mecode.devices.keyence_micrometer import KeyenceMicrometer
 from mecode.devices.efd_pressure_box import EFDPressureBox
 from mecode import G
+from mecode.utils import profile_surface , write_cal_file
 kp = KeyenceProfilometer('COM3') 
 km = KeyenceMicrometer('COM8')
 pb = EFDPressureBox('COM4')
@@ -24,34 +25,34 @@ def find_profilometer_center(kp, axis, zStart, step, dwell, speed, floor):
         value = kp.read()
         pos = g.get_axis_pos(axis=axis)
         if abs(pos - step)>abs(floor):
-            value = None
+            raise RuntimeError('Profilometer about to exceed set floor')
     g.dwell(1.5)
     value = kp.read()
     g.move(**{axis:value})
     g.dwell(1.5)
     value = kp.read()
-    profilometer_middle = g.get_axis_pos(axis=axis)
-    return (profilometer_middle)
+    profilometer_middle = g.get_axis_pos(axis=axis)+value
+    return profilometer_middle
     
-def get_z_ref(kp, x, y, zStart,  axis = 'D', find_mid = 'Yes'):
+def get_z_ref(kp, x, y, zStart,  axis = 'D', find_mid = True):
     g.feed(40)
     g.abs_move(x=x, y=y)
     g.feed(10)
     g.abs_move(**{axis:zStart})
     profilometer_middle = zStart
-    if find_mid == 'Yes':
+    if find_mid == True:
         profilometer_middle = find_profilometer_center(kp, axis = 'D', zStart = zStart, step = 1, dwell = 0.1,  speed = 5, floor = -100)    
     g.abs_move(**{axis:profilometer_middle})
     g.dwell(1.5)
     value = kp.read()
     profilometer_middle = g.get_axis_pos(axis=axis)
     profilometer_ref = profilometer_middle + value
-    return (profilometer_ref)
-    
-def edge_find(kp, xStart, yStart, zStart, step1, step2, backstep, dwell, speed1, speed2, tolerance,  direction='+x', find_mid = 'Yes', axis='D'):
+    return (profilometer_ref)   
+            
+def edge_find(kp, xStart, yStart, zStart, step1, step2, backstep, dwell, speed1, speed2, tolerance,  direction='+x', find_mid = True, axis='D'):
     g.feed(40)
     g.abs_move(x=xStart, y=yStart)
-    if find_mid == 'Yes':
+    if find_mid == True:
         profilometer_middle = find_profilometer_center(kp, axis = axis, zStart = zStart, step = 1, dwell = 0.1, speed = 5, floor = -100)
     xstep1=0
     xstep2=0
@@ -59,27 +60,22 @@ def edge_find(kp, xStart, yStart, zStart, step1, step2, backstep, dwell, speed1,
     ystep2=0
     xbackstep=0
     ybackstep=0
-    indicator = 0
     if direction == '+x':
         xstep1=step1
         xstep2=step2
         xbackstep=-backstep
-        indicator = 1
     elif direction =='-x':
         xstep1=-step1
         xstep2=-step2
         xbackstep=backstep
-        indicator = 1
     elif direction =='+y':
         ystep1=step1
         ystep2=step2 
         ybackstep=-backstep
-        indicator = 0
     elif direction =='-y':
         ystep1=-step1
         ystep2=-step2 
         ybackstep=backstep
-        indicator = 0
     else:
         raise RuntimeError('invalid direction: {}'.format(direction))
     value = kp.read()
@@ -98,10 +94,7 @@ def edge_find(kp, xStart, yStart, zStart, step1, step2, backstep, dwell, speed1,
         value = kp.read()
         if value is None:
             value = tolerance + 1
-    if indicator == 1:
-        coordinate = g.get_axis_pos(axis='x')
-    elif indicator ==0:
-        coordinate = g.get_axis_pos(axis='y')
+    coordinate = g.get_axis_pos(axis=direction[1])
     return coordinate
 
 def get_xyz_offset(km, axis, x, y, zStart, floor, speed_fast, speed_slow, zStep1, zStep2, backstep, downstep, dwell, sweep_range, sweep_speed):
@@ -161,37 +154,22 @@ def get_xyz_offset(km, axis, x, y, zStart, floor, speed_fast, speed_slow, zStep1
     #to get out (x,x_offset), (y, y_offset), (z_axis_position, z_min)=get_xyz_offset()
 
 def xy_align_profilometer(kp, axis='D'):
-    topY = edge_find(kp, xStart=39.5, yStart=330.5, zStart =-88 , step1 = 0.5 , step2 = 0.05, backstep=1, dwell = 0.2, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='-y', find_mid = 'Yes', axis = axis) 
-    leftX = edge_find(kp, xStart=73, yStart=319, zStart =-88 , step1 = 0.5 , step2 = 0.05, backstep=1, dwell = 0.2, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='-x', find_mid = 'No', axis = axis)
+    topY = edge_find(kp, xStart=39.5, yStart=330.5, zStart =-88 , step1 = 0.5 , step2 = 0.05, backstep=1, dwell = 0.2, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='-y', find_mid = True, axis = axis) 
+    leftX = edge_find(kp, xStart=73, yStart=319, zStart =-88 , step1 = 0.5 , step2 = 0.05, backstep=1, dwell = 0.2, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='-x', find_mid = False, axis = axis)
     return (float(leftX), float(topY))
-    #edge_find(kp, xStart, yStart, zStart, step1, step2, backstep, dwell, speed1, speed2, tolerance,  direction='+x', find_mid = 'Yes', axis='D')
+    
 def identify_substrate_location(kp, xStart, yStart, zStart, axis, edges = 'All'):
     g.feed(40)
     g.abs_move(x=xStart, y=yStart)   
     g.feed(20)
     g.abs_move(**{axis:zStart})
     profilometer_middle = find_profilometer_center(kp, axis = axis, zStart = zStart, step = 1, dwell = 0.1,  speed = 5, floor = -100)
-    leftX= edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.4 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 3, speed2 = 1, tolerance = 0.15, direction='-x', find_mid = 'No')
-    topY = edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.4 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 3, speed2 = 1, tolerance = 0.15, direction='+y', find_mid = 'No')
-    #rightX= edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.75 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='+x', find_mid = 'No')
-    #bottomY= edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.75 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='-y', find_mid = 'No')
-    return (leftX, topY), (profilometer_middle)
-    
-def get_substrate_ref(kp, xRef, yRef, zStart, axis = 'D', find_center = 'Yes'):            
-    g.feed(40)
-    g.abs_move(x=xRef, y=yRef)
-    g.feed(10)
-    g.abs_move(**{axis:zStart})
-    if find_center == 'Yes':
-        profilometer_middle = find_profilometer_center(kp, axis = axis, zStart = zStart, step = 1, dwell = 0.1,  speed = 5, floor = -100)        
-    g.dwell(0.75)
-    value = kp.read() 
-    g.move(**{axis:-value})
-    g.dwell(1)
-    z_axis_position = g.get_axis_pos(axis=axis)
-    value = kp.read() 
-    prof_substrate = z_axis_position + value
-    return (prof_substrate)
+    leftX= edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.4 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 3, speed2 = 1, tolerance = 0.15, direction='-x', find_mid = False)
+    bottomY = edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.4 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 3, speed2 = 1, tolerance = 0.15, direction='-y', find_mid = False)
+    #rightX= edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.75 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='+x', find_mid = False)
+    #bottomY= edge_find(kp, xStart=xStart, yStart=yStart, zStart =profilometer_middle , step1 = 0.75 , step2 = 0.1, backstep=10, dwell = 0, speed1 = 5, speed2 = 1, tolerance = 0.15, direction='-y', find_mid = False)
+    return (leftX, bottomY), (profilometer_middle)
+
 
 
 
@@ -332,23 +310,22 @@ def run_alignment_script(align_A, align_B, align_C, align_D,  zSensor_Plate_offs
    g.abs_move(**{axis:-40})
    
    #Profile z_ref location
-   (ref_prof_position) = get_z_ref(kp, x = x_zref , y=y_zref , zStart=start_zref , axis = axis, find_mid = 'Yes')
+   (ref_prof_position) = get_z_ref(kp, x = x_zref , y=y_zref , zStart=start_zref , axis = axis, find_mid = True)
    
    g.feed(25)
    g.abs_move(**{axis:-40})
    
    #find substrate boundaries
-   (substrate_left_x, substrate_top_y), (prof_substrate_start)=  identify_substrate_location(kp, xStart = 51.20, yStart = 36.46, zStart = -89.9, axis = axis, edges = 'All')
+   (substrate_left_x, substrate_bottom_y), (prof_substrate_start)=  identify_substrate_location(kp, xStart = 51.20, yStart = 36.46, zStart = -89.9, axis = axis, edges = 'All')
    
    g.feed(25)
    g.abs_move(**{axis:-40})
    
    #move to substrate reference and obtain profilometer ref
-   (substrate_z_pos) = get_substrate_ref(kp, xRef= substrate_left_x + Substrate_xRef, yRef=substrate_top_y + Substrate_yRef, zStart=prof_substrate_start, axis = axis)
-   
-   g.feed(25)
-   g.abs_move(**{axis:-40})
-   
+   (substrate_z_pos) = get_z_ref(kp, x= substrate_left_x + Substrate_xRef, y=substrate_bottom_y + Substrate_yRef, zStart=prof_substrate_start, axis = axis, find_mid=False)
+   #g.feed(25)
+   #g.abs_move(**{axis:-40})
+   #
    print '!!!!!! right before calc', profilometer_y_groove_new, type(profilometer_y_groove_new)
    #calculate relative axis xyz positions
    (zA, Ax_groove, Ay_groove), (zB, Bx_groove, By_groove), (zC, Cx_groove, Cy_groove), (zD, Dx_groove, Dy_groove) = calculate_xyz_pos(Az_axis_position,  Az_min,  Ax,  Ax_offset,  Ay,  Ay_offset,  Bz_axis_position,  Bz_min,  Bx,  Bx_offset, 
@@ -356,11 +333,14 @@ def run_alignment_script(align_A, align_B, align_C, align_D,  zSensor_Plate_offs
    Dx, Dx_offset, Dy, Dy_offset, profilometer_x_groove_new, profilometer_y_groove_new, ref_prof_position, substrate_z_pos, zSensor_Plate_offset,  prof_ref_cal , 
    Rz_axis_position , Rz_min, Rx_offset, Rx , Ry, Ry_offset, Rx_groove , Ry_groove , profilometer_x_groove_old , profilometer_y_groove_old )
    
-   nozzle_change(Ax_groove, Ay_groove, Bx_groove, By_groove, Cx_groove, Cy_groove, 
-            Dx_groove, Dy_groove, profilometer_x_groove_new, profilometer_y_groove_new, nozzles = 'pa')
+   #nozzle_change(Ax_groove, Ay_groove, Bx_groove, By_groove, Cx_groove, Cy_groove, 
+            #Dx_groove, Dy_groove, profilometer_x_groove_new, profilometer_y_groove_new, nozzles = 'pa')
+   
+   x_offset = (Ax_groove - profilometer_x_groove_new)
+   y_offset = (Ay_groove - profilometer_y_groove_new)
    
    #g.move(x=-30, y=10)
-   g.abs_move(A=(zA + 0.04))
+   #g.abs_move(A=(zA + 0.04))
    #g.set_valve(num = 0, value = 0)
    #pb.set_pressure(com_port = 4, value = 5)
    #pb.toggle_pressure(4)
@@ -386,14 +366,20 @@ def run_alignment_script(align_A, align_B, align_C, align_D,  zSensor_Plate_offs
    #load cal file
    
    # kp.disconnect  - Closes comport
-   print '(Ax,Ax_offset), (Ay, Ay_offset), (Az_axis_position, Az_min): ' ,  (Ax,Ax_offset), (Ay, Ay_offset), (Az_axis_position, Az_min)
-   print 'prof grooves location: ' , (profilometer_x_groove_new, profilometer_y_groove_new)
-   print 'prof ref z: ' ,  (ref_prof_position)
+   print '(x_offset), (y_offset): ' ,  x_offset, y_offset
+   print 'substrate_left_x, substrate_bottom_y: ' ,  substrate_left_x, substrate_bottom_y
+   #print '(Ax,Ax_offset), (Ay, Ay_offset), (Az_axis_position, Az_min): ' ,  (Ax,Ax_offset), (Ay, Ay_offset), (Az_axis_position, Az_min)
+   #print 'prof grooves location: ' , (profilometer_x_groove_new, profilometer_y_groove_new)
+   #print 'prof ref z: ' ,  (ref_prof_position)
    #print 'substrate orgin: '(substrate_left_x, substrate_top_y)
    #print 'prof substrate z: ' (substrate_z_pos)
-
+   return x_offset, y_offset, substrate_left_x, substrate_bottom_y
   
-run_alignment_script(align_A='yes', align_B='no', align_C='no', align_D='no', zSensor_Plate_offset = 42.8854, prof_ref_cal = 96.03958, Rz_axis_position = -43.099992493421, Rz_min=+02.82200, Rx_offset=1.7944, Rx = 586.075, Ry = 367.82, Ry_offset = 0.64815, Rx_groove = 400.2418, Ry_groove = 342.4803, Substrate_xRef = 37, Substrate_yRef = -26, x_zref = 54.588, y_zref = 280.322 , profilometer_x_groove_old = 70.4999, profilometer_y_groove_old = 327.850046, start_zref = -94, axis = 'D')         
+x_offset, y_offset, substrate_left_x, substrate_bottom_y =run_alignment_script(align_A='yes', align_B='no', align_C='no', align_D='no', zSensor_Plate_offset = 42.8854, prof_ref_cal = 96.03958, Rz_axis_position = -43.099992493421, Rz_min=+02.82200, Rx_offset=1.7944, Rx = 586.075, Ry = 367.82, Ry_offset = 0.64815, Rx_groove = 400.2418, Ry_groove = 342.4803, Substrate_xRef = 37, Substrate_yRef = 26, x_zref = 54.588, y_zref = 280.322 , profilometer_x_groove_old = 70.4999, profilometer_y_groove_old = 327.850046, start_zref = -94, axis = 'D')         
+surface = profile_surface(g, kp, x_start=substrate_left_x + 2, x_stop = substrate_left_x + 62, x_step = 10, y_start=substrate_bottom_y + 2, y_stop = substrate_bottom_y + 52, y_step = 10)
+
+write_cal_file('C:\Users\Lewis Group\Desktop\Calibration\CAL_output.cal', surface, x_start= substrate_left_x + 2, x_stop = substrate_left_x + 62, x_step = 10, y_start = substrate_bottom_y + 2, y_stop = substrate_bottom_y + 52,
+                   y_step = 10, x_offset = x_offset, y_offset = y_offset, mode='w+', ref_zero=True)
 #(substrate_left_x, substrate_top_y), (substrate_right_x, substrate_bottom_y), (prof_substrate_start)=  identify_substrate_location(kp, xStart = 51.20, yStart = 36.46, zStart = -89.9, axis = 'D', edges = 'All')   
 #print (substrate_left_x, substrate_top_y)   
 #(profilometer_x_groove_new, profilometer_y_groove_new) = xy_align_profilometer(kp, axis = 'D')
